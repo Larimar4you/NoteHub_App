3abd551f3aaae140186716e4cece4af94e9c43ed
Larimar4you <Zajanezlaja@gmail.com>
2026-04-08 00:31:29 +0200

    GO

---
+++ src/components/App/App.tsx
@@ -0,0 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+import css from "./App.module.css";
+import { fetchNotes } from "../../services/noteService";
+import NoteList from "../NoteList/NoteList";
+
+function App() {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["notes"],
+    queryFn: () => fetchNotes({ page: 1 }),
+  });
+
+  const notes = data?.notes ?? [];
+
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        {/* SearchBox */}
+        {/* Pagination */}
+        {/* Create button */}
+      </header>
+
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Error...</p>}
+
+      {/* умова завдання */}
+      {notes.length > 0 && <NoteList notes={notes} />}
+    </div>
+  );
+}
+
+export default App;
